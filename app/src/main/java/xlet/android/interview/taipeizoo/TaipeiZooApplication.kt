28493cfecf390cccd493ee901abf7aa2e645b754
Lionel <cwhsiao.lionel@gmail.com>
2023-03-24 19:52:25 +0800

    feat: Add DI plugin (hilt)
    Signed-off-by: Lionel <cwhsiao.lionel@gmail.com>

---
+++ app/src/main/java/xlet/android/interview/taipeizoo/TaipeiZooApplication.kt
@@ -0,0 +1,7 @@
+package xlet.android.interview.taipeizoo
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TaipeiZooApplication : Application()
--- app/src/main/java/xlet/android/interview/taipeizoo/ui/MainActivity.kt
+++ app/src/main/java/xlet/android/interview/taipeizoo/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package xlet.android.interview.taipeizoo
+package xlet.android.interview.taipeizoo.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import xlet.android.interview.taipeizoo.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
